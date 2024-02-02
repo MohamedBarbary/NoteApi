74d98a25d8ed202947b6eb0f8d35c3f2351cc343
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2024-02-02 17:09:28 +0200

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -15,15 +15,14 @@ const signToken = (id) => {
 };
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
-  //   expires: new Date(
-  //     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-  //   ),
-  //   httpOnly: true,
-  //     sameSite: 'None',
-  // };
-  // cookieOptions.secure = true;
-  // // if (process.env.NV === 'production') cookieOptions.secure = true;
+  // const cookieOptions = {
+  //  expires: new Date(
+  //      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  //    ),
+  //    httpOnly: true,
+  //      sameSite: 'None',
+  //  };
+  //  cookieOptions.secure = true;
 
   res.cookie('jwt', token);
 
