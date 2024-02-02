b3561674ffccb7a9327e8eee44a43c9cf462b48f
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2024-02-02 18:10:03 +0200

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -15,21 +15,7 @@ const signToken = (id) => {
 };
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  // const cookieOptions = {
-  //  expires: new Date(
-  //      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-  //    ),
-  //    httpOnly: true,
-  //      sameSite: 'None',
-  //  };
-  //  cookieOptions.secure = true;
-
-  res.cookie('jwt', token ,{
-  path: "/",
-  httpOnly: true,
-  secure: false,
-  maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
-});
+  res.cookie('jwt', token );
 
   // Remove password from output
   user.password = undefined;
