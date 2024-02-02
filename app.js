a5fafa1bfbeef28daad4ac46b56dc191e9d3883d
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2024-02-02 17:32:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const globalErrorHandler = require('./controller/errorController');
@@ -9,12 +10,12 @@ const userRouter = require('./routes/userRouter');
 const noteRouter = require('./routes/noteRouter');
 const AppError = require('./utils/appError');
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Replace * with specific origin if needed
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(
+  cors({
+    credentials: true,
+    origin: '*'
+  })
+);
 
 app.use(helmet());
 // app.use(
