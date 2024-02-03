83705daf7cba35c2101c7bda4e7d2baab10400ec
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2024-02-03 19:35:28 +0200

    Update errorController.js

--- controller/errorController.js
+++ controller/errorController.js
@@ -53,17 +53,6 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  // if (process.env.NODE_ENV === 'development') {
   sendErrorDev(err, res);
-  // } else if (process.env.NODE_ENV === 'production') {
-  //   let error = { ...err };
-
-  //   if (error.name === 'CastError') error = handleCastErrorDB(error);
-  //   if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-  //   if (error.name === 'ValidationError')
-  //     error = handleValidationErrorDB(error);
-  //   if (error.name === 'JsonWebTokenError') error = handleJWTError();
-  //   if (error.name === 'TokenExpiredError') error = handleTokenExpired();
-  //   sendErrorProd(error, res);
-  // }
+  
 };
